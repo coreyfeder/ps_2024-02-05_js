2b13201b10b194a5b9b3f318a0b5566c37fb34ee
Corey Feder <corey.feder@gmail.com>
2024-02-05 10:39:01 -0500

    sample js outputs

--- script.js
+++ script.js
@@ -4,3 +4,10 @@
  */
 
 console.log("Script running.")
+
+console.log("data types: number: integer: ", 9)
+console.log("data types: number: float: ", 9.9)
+console.log("data types: string: ", "nine")
+console.log("data types: boolean: ", true)
+console.log("data types: null: ", null)
+console.log("data types: undefined: ", 0/0)
