35fda3c971a1516571698f795a0bbad313a40915
Corey Feder <corey.feder@gmail.com>
2024-02-05 19:32:22 -0500

    break down problem

--- ALAB 308.1.1.js
+++ ALAB 308.1.1.js
@@ -1,3 +1,18 @@
 // We have four numbers that need to add up to 50. At least two of these numbers must 
 // be odd. No number can be larger than 25, and each number must be unique.
 
+/*
+ * Given:
+ * - we have four inputs
+ * 
+ * Implied:
+ * - the inputs are numbers
+ * - the numbers are rational
+ * - the numbers are integers
+ * 
+ * Confirm:
+ * - the sum of the inputs is 50
+ * - two or more inputs are odd
+ * - numbers are ≤ 25
+ * - numbers are unique
+ */
